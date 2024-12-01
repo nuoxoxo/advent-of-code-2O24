9294b106a394db8984f3e8efdab76133bd0b4104
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-12-01 10:02:38 +0100

    1/done

---
+++ 01.py
@@ -0,0 +1,13 @@
+lines = open(0).read().strip().split('\n')
+
+L, R = [], []
+for line in lines:
+    l, r = [int(_) for _ in line.split()]
+    L.append(l)
+    R.append(r)
+A, B = 0, 0
+for l, r in zip(sorted(L), sorted(R)):
+    A += abs(l - r)
+    B += R.count(l)* l 
+print('part 1:', A)
+print('part 2:', B)
