52a9f81c56aa1ebc3bd62778aea0f83a5cd89d41
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-12-02 19:06:45 +0100

    1/minor

--- 02.py
+++ 02.py
@@ -18,11 +18,10 @@ for line in lines:
 
     if ok (a):
         r1 += 1
-        r2 += 1
     else:
         for i in range(len(a) - 1):
             aa = a[:i] + a[i + 1:]
             if ok (aa):
                 r2 += 1
 print('part 1:', r1)
-print('part 1:', r2)
+print('part 2:', r1 + r2)
