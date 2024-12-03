dadae640064e47a746c311e96fbf4a06e28b81da
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-12-03 22:18:40 +0100

    1/using *

--- 01.py
+++ 01.py
@@ -1,10 +1,14 @@
 lines = open(0).read().strip().split('\n')
 
+L, R = zip(*(map(int, _.split()) for _ in lines))
+# simplest way to get L R
+"""
 L, R = [], []
 for line in lines:
     l, r = [int(_) for _ in line.split()]
     L.append(l)
     R.append(r)
+"""
 A, B = 0, 0
 for l, r in zip(sorted(L), sorted(R)):
     A += abs(l - r)
