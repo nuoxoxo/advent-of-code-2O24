c44bfcfcc667d37a866bf75e4280feced22d3029
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-12-03 18:46:39 +0100

    3/redo

--- 03.py
+++ 03.py
@@ -2,16 +2,23 @@ import re
 lines = open(0).read().strip().split('\n')
 r2,r1 = 0,0
 ok = True
-for l in lines:
-    for i in range(len(l)):
-        sub = l[i:]
-        if sub.startswith('do()'): ok = True
-        if sub.startswith('don\'t()'): ok = False
-        if l[i:].startswith('mul('):
-            m = re.match(r'mul\((\d+,\d+)\)', l[i:])
-            if not m: continue
-            L, R = [int(_)for _ in m.group(1).split(',')]
-            r1 += L*R
-            r2 += L * R if ok else 0
-print(len(lines), r1, r2)
 
+for l in lines:
+    # p1
+    ps = re.findall(r'mul\((\d+,\d+)\)', l)
+    for p in ps:#pairs:
+        L, R = [int(_)for _ in p.split(',')]
+        r1 += L * R
+    # p2
+    ts = re.findall(r'mul\((\d+,\d+)\)|(do\(\))|(don\'t\(\))', l)
+    for a, b, c in ts:#trios:
+        if b: ok = True
+        if c: ok = False
+        if ok and a:
+            L, R = [int(_)for _ in a.split(',')]
+            r2 += L * R
+
+print('part 1:', r1)
+print('part 2:', r2)
+assert(r1 == 160672468)
+assert(r2 == 84893551)
