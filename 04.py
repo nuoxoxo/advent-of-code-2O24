2d49173915c5a0449509c588d2a0735a6cee71ab
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-12-04 23:19:22 +0100

    4/better

--- 04.py
+++ 04.py
@@ -1,35 +1,12 @@
 L = open(0).read().splitlines()
 rev = [''.join(_) for _ in list(zip(*L))]
-t = 'XMAS'
 R, C = len(L), len(L[0])
+t = 'XMAS'
 r2,r1=0,0
 
-# p2
-def grid(G) -> bool:
-    out = G[0][0] + G[0][-1] + G[-1][-1] + G[-1][0]
-    return G[1][1] == 'A' and out in ['MSSM','SSMM','SMMS','MMSS']
-
-for r in range(R - 2):
-    for c in range(C - 2):
-        sub = []
-        for rr in range(r, r + 3):
-            sub.append(L[rr][c:c + 3])
-        if grid(sub):
-            r2 += 1
 # p1
-D = [(1,1),(1,-1)]
-def hove(s:str) -> int:
-    """
-    res = 0
-    for i in range(len(s) - 3):
-        sub = s[i:i + 4]
-        if sub == t or sub == t[::-1]:#Counter(sub) == Counter(t):
-            res += 1
-    return res
-    """
-    return s.count('XMAS') + s.count('SAMX')
-
 def diag(r:int, c:int) -> int: 
+    D = [(1,1),(1,-1)]
     res = 0
     for dr, dc in D:
         rr = r
@@ -41,15 +18,29 @@ def diag(r:int, c:int) -> int:
             if not (-1<rr<R and -1<cc<C):
                 break
             sub += L[rr][cc]
-        if len(sub) == 4 and sub == t or sub == t[::-1]:
-            res += 1
+        res += sub == t or sub == t[::-1]
     return res
 
 for l,r in zip(L, rev):
-    r1 += hove(l) + hove(r)
+    r1 += l.count(t) + l.count(t[::-1]) + r.count(t) + r.count(t[::-1])
 for r in range(R):
     for c in range(C):
         r1 += diag(r,c)
+
+# p2
+def p2(g) -> bool:
+    if g[1][1] != 'A':
+        return False
+    s = g[0][0] + g[0][-1] + g[-1][-1] + g[-1][0]
+    return s.count('S') == 2 and s.count('M') == 2 \
+        and ('SS' in s or 'MM' in s)
+
+for r in range(R - 2):
+    for c in range(C - 2):
+        a = []
+        for rr in range(r, r + 3):
+            a.append(L[rr][c:c + 3])
+        r2 += p2(a)
 
 print('part 1:', r1)
 print('part 2:', r2)
