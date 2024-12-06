2004cddad46282859c9a941f0f39bbf8bd3246b0
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-12-06 17:05:01 +0100

    6/upd

--- 06.py
+++ 06.py
@@ -15,29 +15,22 @@ def p2(g, rr: int, cc: int) -> bool:
     r,c=sr,sc
     D = [ (-1,0), (0,1), (1,0), (0,-1) ]
     i = 0
-    SEEN = set([(sr,sc,i)])
+    S = set([(sr,sc,i)])
     while 42:
         dr,dc = D[i]
         rr, cc = r + dr, c + dc
         if not (-1<rr<R and -1<cc<C):
             return False
         if g[rr][cc] == '#':
-            i += 1
-            if i > 3:
-                i = 0
+            i = (i + 1) % 4
         else:
             r,c = rr,cc
-        if (rr,cc,i) in SEEN:
+        if (rr,cc,i) in S:
             return True
-        SEEN.add((r,c,i))
+        S.add((r,c,i))
     assert(False)
-r2 = 0
-for r in range(R):
-    for c in range(C):
-        if g[r][c] not in ['#', '^']:
-            r2 += p2(g[:],r,c)
 
-# part 1
+# part 1 and 2
 r,c=sr,sc
 SEEN = set([(sr,sc)])
 D = [ (-1,0), (0,1), (1,0), (0,-1) ]
@@ -49,11 +42,14 @@ while 42:
         #print('p1/end', len(SEEN))
         break
     if g[rr][cc] == '#':
-        i += 1
-        if i > 3:
-            i = 0
+        i = (i + 1) % 4
     else:
         r,c = rr,cc
         SEEN.add((r,c))
+
+r2 = 0
+for r, c in SEEN:
+    r2 += p2(g[:],r,c)
+
 print('part 1:', len(SEEN))
 print('part 2:', r2)
