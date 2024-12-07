cbf269fad6138626d1426fa96f1d34b831a3971a
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-12-08 00:57:46 +0100

    1/upd p1

--- 01.sh
+++ 01.sh
@@ -1,37 +1,40 @@
 #!/bin/bash
+
+FILE=1.0
 L=()
 R=()
 while read -r l r; do
     L+=("$l")
     R+=("$r")
-done
-
-# use printf to print them all out, piping into sort -n
+done < "$FILE"
 L=($(printf "%s\n" "${L[@]}" | sort -n))
 R=($(printf "%s\n" "${R[@]}" | sort -n))
-
-# get the len assuming both lists have the same length
 N=${#L[@]}
-
-# DBG
-#echo "L: ${L[@]}"
-#echo "R: ${R[@]}"
-
-A=0
-B=0
+p1=0
+p2=0
 for ((i = 0; i < N; i++)); do
     tmp=$((L[i] - R[i]))
     tmp=$((tmp < 0 ? -tmp : tmp))
-    A=$((A + tmp))
+    p1=$((p1 + tmp))
     occur=0
     for r in "${R[@]}"; do
         if [[ "${L[i]}" -eq "$r" ]]; then
             occur=$((occur+1))
         fi
     done
-    B=$((B+occur*L[i]))
+    p2=$((p2+occur*L[i]))
 done
+echo "part 1: $p1" 
+echo "part 2: $p2" 
 
-echo "part 1: $A" 
-echo "part 2: $B" 
+echo "------2nd way------"
 
+p1=$(paste -d '-' \
+    <(tr -s ' ' < "$FILE" | cut -d ' ' -f1 | sort -n) \
+    <(tr -s ' ' < "$FILE" | cut -d ' ' -f2 | sort -n) \
+    | { p1=0; while read -r l r; do
+        diff=$((l - r))
+        diff=$(echo "$diff" | tr -d '-')
+        p1=$((p1 + diff))
+    done; echo "$p1"; })
+echo "part 1: $p1"
