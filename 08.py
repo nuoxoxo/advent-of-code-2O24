bcf51a424f2efb9925f94821e89f6556ac8b63a3
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-12-08 18:15:46 +0100

    sh/both parts - quite fast

--- 08.py
+++ 08.py
@@ -9,21 +9,21 @@ for r, row in enumerate(g):
 #print(A)
 nodes = set()
 nodes2 = set()
-for a, cset in A.items():
+for cset in A.values():
     coor = sorted(list(cset))
     for r,c in coor:
-        #print(a, l,r,c)
         for rr, cc in coor:
+            if rr == r and cc == c: continue
             dr, dc = rr - r, cc - c
-            if dr == 0 and dc == 0:continue
             if r - dr in range(R) and c - dc in range(C):
                 nodes.add((r - dr, c - dc))
             if rr + dr in range(R)and cc + dc in range(C):
                 nodes.add((rr + dr, cc + dc))
             # PART 2
             i, j = r, c
-            t = 0
+            t = -1
             while 42:
+                t += 1
                 found = False
                 if i + dr * t in range(R) and j + dc * t in range(C):
                     nodes2.add((i + dr * t, j + dc * t))
@@ -32,7 +32,5 @@ for a, cset in A.items():
                     nodes2.add((i - dr * t, j - dc * t))
                     found = True
                 if not found: break
-                t += 1
-
 print('part 1:', len(nodes))
 print('part 2:', len(nodes2))
---
+++ 08.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+g=()
+while IFS= read -r line; do
+    g+=("$line")
+done
+
+declare -A A  
+R=${#g[@]}
+C=${#g[0]}
+
+for (( r = 0; r < R; r++ )); do
+    for (( c = 0; c < C; c++ )); do
+        char=${g[r]:c:1}
+        if [[ "$char" != "." ]]; then
+            A["$char"]+="($r,$c) "
+        fi
+    done
+done
+#for k in "${!A[@]}"; do echo "dbg/ key $k, set: ${A[$k]}";done
+
+declare -A btw
+declare -A ALL
+for atn in "${!A[@]}"; do
+    coor="${A["$atn"]}"
+    #echo; echo "$atn - $coor" # ok/
+
+    for tup in $coor; do
+        tup=${tup//[()]/}
+        sr=${tup%,*}
+        sc=${tup#*,}
+        #echo; echo "sr,sc/ $sr, $sc" # ok/
+        for cmp in $coor; do
+            cmp=${cmp//[()]/}
+            er=${cmp%,*}
+            ec=${cmp#*,}
+            if [[ "$sr" == "$er" && "$sc" == "$ec" ]]; then
+                continue
+            fi
+            #echo "er,ec/ $er, $ec" # ok/
+            dr=$(( er - sr ))
+            dc=$(( ec - sc ))
+            # PART 1
+            rr=$(( sr - dr ))
+            cc=$(( sc - dc ))
+            if (( -1 < rr && rr < R && -1 < cc && cc < C )); then
+                btw["($rr,$cc)"]=1
+            fi
+            rr=$(( er + dr ))
+            cc=$(( ec + dc ))
+            if (( -1 < rr && rr < R && -1 < cc && cc < C )); then
+                btw["($rr,$cc)"]=1
+            fi
+            # PART 2
+            t=0
+            while true; do
+                changed=false
+                rr=$(( er + dr * t ))
+                cc=$(( ec + dc * t ))
+                if (( -1 < rr && rr < R && -1 < cc && cc < C )); then
+                    ALL["($rr,$cc)"]=1
+                    changed=true
+                fi
+                rr=$(( sr - dr * t ))
+                cc=$(( sc - dc * t ))
+                if (( -1 < rr && rr < R && -1 < cc && cc < C )); then
+                    ALL["($rr,$cc)"]=1
+                    changed=true
+                fi
+                if [[ "$changed" == false ]]; then
+                    break
+                fi
+                (( t++ ))
+            done
+        done
+    done
+done
+#for k in "${!btw[@]}";do echo"dbg/ key $k, set: ${btw[$k]}";done
+
+echo "part 1/ ${#btw[@]}"
+echo "part 2/ ${#ALL[@]}"
+
