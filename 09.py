ff6930359c11d0d0ee070d3226371bdb93e6c005
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-12-10 16:49:54 +0100

    p1/done

--- 09.py
+++ 09.py
@@ -1,31 +1,36 @@
 line = open(0).read().strip()
-#line = '2333133121414131402'#'12345'
+line = '2333133121414131402'#'12345'
+
+def makeQ():
+    q = []
+    curr = 0
+    for i, c in enumerate(line):
+        if i % 2 == 0:
+            for _ in range(int(c)): q.append(curr)
+            curr += 1
+            continue
+        for _ in range(int(c)): q.append('.')
+    return q
 
 def p2() -> int:
     res = 0
+    Q = makeQ()
     return res
 
-Q = []
-curr = 0
-for i, c in enumerate(line):
-    if i % 2 == 0:
-        for _ in range(int(c)):
-            Q.append(curr)
-        curr += 1
-    else:
-        for _ in range(int(c)):
-            Q.append(None)
+part2 = p2()
 
-i = 0;#print(Q)
-while i < len(Q):
-    if Q[i] is not None:
-        i += 1
-    else:
-        while Q[i] is None:
+# p1
+
+def p1() -> int:
+    Q = makeQ();print('a/', ''.join(str(_) for _ in Q))
+    i = 0;
+    while i < len(Q):
+        while Q[i] == '.':
             Q[i] = Q.pop()
         i += 1
+    print('a/', ''.join(str(_) for _ in Q))
+    return sum( i * n for i, n in enumerate(Q))
+part1 = p1()
 
-part1 = sum( i * n for i, n in enumerate(Q))
-part2 = p2()
 print('part 1:', part1); assert(part1 % 1000 in [548, 928])
 print('part 2:', part2); assert(part2 % 1000 in [311, 858])
