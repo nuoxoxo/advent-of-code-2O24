fbd94332148bdfecf293a7a5f0faa51c792746b2
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-12-10 23:10:49 +0100

    9/redo - minor

--- 09.py
+++ 09.py
@@ -12,7 +12,7 @@ def makeQ():
         for _ in range(int(c)): q.append('.')
     return q
 
-def p2() -> int:
+def part2() -> int:
     res = 0
     Q = makeQ();print(''.join(str(_) for _ in Q), '/B\n')
     i = 0
@@ -20,7 +20,6 @@ def p2() -> int:
     N = len(Q)
     highest = None
     while 42:
-        # counting spaces
         if Q[i] == '.':
             count = 0
             start = i
@@ -30,51 +29,33 @@ def p2() -> int:
                 SPACE.append( (start, count) )
             if i >= N:
                 break
-        # counting numbers
         else:
             count = 0
             start, curr = i, Q[i]
             while i < N and Q[i] == curr:
                 count, i = count + 1, i + 1
             if count != 0:
-                #IDS.append( (curr, start, count) )
                 IDS[int(curr)] = ( start, count )
             highest = int(curr)
             if i >= N:
                 break
-    # moving whole files
-    highest_saved = highest
+    # moving files
     while highest > -1:
         left, numsize = IDS[highest]
         for i, (start,spacesize) in enumerate(SPACE):
             if start > left: break
             if numsize == spacesize:
-                IDS[highest] = ( start, numsize )# update IDS
-                SPACE[i] = ( start + numsize, 0 )# update SPACE
+                IDS[highest] = (start, numsize)
+                SPACE[i] = (start + numsize, 0)
                 break
             if numsize < spacesize:
-                IDS[highest] = ( start, numsize )# upd IDS
-                SPACE[i] = ( start + numsize, spacesize - numsize )# upd SPACE
+                IDS[highest] = (start, numsize)
+                SPACE[i] =(start + numsize, spacesize - numsize)
                 break
         highest -= 1
-    i = 0
-    for k, (start, size) in IDS.items():
-        for j in range(start, start + size):
-            res += j * k
-    return res
+    return sum( i * n for n, (L, N) in IDS.items() for i in range(L, L + N))
 
-part2 = p2()
-
-print('sample/\n' + """00...111...2...333.44.5555.6666.777.888899
-0099.111...2...333.44.5555.6666.777.8888..
-0099.1117772...333.44.5555.6666.....8888..
-0099.111777244.333....5555.6666.....8888..
-00992111777.44.333....5555.6666.....8888..
-""")
-
-# p1
-
-def p1() -> int:
+def part1() -> int:
     Q = makeQ();print('A/', ''.join(str(_) for _ in Q))
     i = 0;
     while i < len(Q):
@@ -83,9 +64,8 @@ def p1() -> int:
         i += 1
     print('A/', ''.join(str(_) for _ in Q))
     return sum( i * n for i, n in enumerate(Q))
-part1 = p1()
 
-print('part 1:', part1); assert(part1 % 1000 in [548, 928])
-print('part 2:', part2); assert(part2 % 1000 in [311, 858])
-
+p1, p2 = part1(), part2()
+print('part 1:', p1); assert(p1 % 1000 in [548, 928])
+print('part 2:', p2); assert(p2 % 1000 in [311, 858])
 
