0be7fb6ae15ec6e295dcbf50b8b7ca126607f8d2
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-12-12 21:38:04 +0100

    12/done

--- 12.py
+++ 12.py
@@ -1,13 +1,12 @@
 g = open(0).read().splitlines();
-for l in g:print(l)
-R,C = len(g),len(g[0])
-D = ((-1,0),(0,1),(1,0),(0,-1))
+ROW,COL = len(g),len(g[0])
+DIR = ((-1,0),(0,1),(1,0),(0,-1))
 SEEN = set()
-res = 0
-res2 = 0
-from collections import defaultdict,deque
-for r in range(R):
-    for c in range(C):
+p1 = 0
+p2 = 0
+from collections import defaultdict, deque
+for r in range(ROW):
+    for c in range(COL):
         if (r,c) not in SEEN:
             plant = g[r][c]
             region = []
@@ -19,74 +18,47 @@ for r in range(R):
                     continue
                 SEEN.add((sr,sc))
                 region.append((sr,sc))
-                for dr,dc in D:
+                for dr,dc in DIR:
                     rr = sr + dr
                     cc = sc + dc
-                    if -1<rr<R and -1<cc<C and (rr,cc) not in SEEN :
+                    if -1<rr<ROW and -1<cc<COL and (rr,cc) not in SEEN :
                         Q.append((rr,cc))
-                    #print(plant,'-', rr,cc,'-',region)
-            print('plant/', plant, '\nregion/', region)
+            # p1
             pmt=0
             fence=0
             for (sr,sc) in region:
-                for dr,dc in D:
+                for dr,dc in DIR:
                     rr,cc = sr + dr, sc + dc
-                    if not (-1<rr<R and -1<cc<C) or g[rr][cc] != plant:
-                        pmt += 1
-            #print('pmt/', plant, pmt)
-            res += pmt * len(region)
+                    pmt += not (-1<rr<ROW and -1<cc<COL) or g[rr][cc] != plant
+            p1 += pmt * len(region)
 
-            ### p2
-
+            # p2
             noup, nodown, noleft, noright = set(),set(),set(),set()
-            UP,DO,LE,RI=defaultdict(list),defaultdict(list),defaultdict(list),defaultdict(list)
-            ## vertical
+            U, D, L, R = \
+                defaultdict(list), defaultdict(list), defaultdict(list), defaultdict(list)
+
+            ### vertical
             for i,j in region:
                 if i-1 < 0 or g[i-1][j] != plant: noup.add((i, j))
-                if i+1 >= R or g[i+1][j] != plant: nodown.add((i, j))
-            print('noup/', sorted(list(noup)))
-            print('nodown/', sorted(list(nodown)))
-            for i,j in noup: UP[i].append(j)
-            for i,j in nodown: DO[i].append(j)
-            for k,v in UP.items():
-                v=sorted(v)
-                print('up/', k, v)
-                fence += 1
-                for i in range(len(v) - 1):
-                    if v[i] + 1 != v[i + 1]: fence += 1
-            print('fence/done up',fence)
-            for k,v in DO.items():
-                v=sorted(v)
-                print('do/', k, v)
-                fence += 1
-                for i in range(len(v) - 1):
-                    if v[i] + 1 != v[i + 1]: fence += 1
-            print('fence/done down',fence)
+                if i+1 >= ROW or g[i+1][j] != plant: nodown.add((i, j))
+            for i,j in noup: U[i].append(j)
+            for i,j in nodown: D[i].append(j)
+
             ### horizontal
             for i,j in region:
                 if j-1 < 0 or g[i][j-1] != plant: noleft.add((i, j))
-                if j+1 >= R or g[i][j+1] != plant: noright.add((i, j))
-            print('noleft/', sorted(list(noleft)))
-            print('noright/', sorted(list(noright)))
-            for i,j in noleft: LE[j].append(i)
-            for i,j in noright: RI[j].append(i)
-            #for k,v in LE.items(): print('le/', k, v)
-            for k,v in LE.items():
-                v=sorted(v)
-                print('left/', k, v)
-                fence += 1
+                if j+1 >= COL or g[i][j+1] != plant: noright.add((i, j))
+            for i,j in noleft: L[j].append(i)
+            for i,j in noright: R[j].append(i)
+
+            ### count continuous fences
+            def fencing(v) -> int:
+                res = 1
                 for i in range(len(v) - 1):
-                    if v[i] + 1 != v[i + 1]: fence += 1
-            print('fence/done left',fence)
-            #for k,v in RI.items(): print('ri/', k, v)
-            for k,v in RI.items():
-                v=sorted(v)
-                print('right/', k, v)
-                fence += 1
-                for i in range(len(v) - 1):
-                    if v[i] + 1 != v[i + 1]: fence += 1
-            print('fence/done right',fence)
-            res2 += fence * len(region);print()
-            #print('fence/', plant, fence);print()
-print(res)
-print(res2)
+                    res += v[i] + 1 != v[i + 1]
+                return res
+            fence = sum([fencing( sorted(_) ) for block in [U, D, L, R] for _ in block.values()])
+            p2 += fence * len(region)
+
+print('part 1:', p1);assert(p1 in [1456082,140,1930])
+print('part 2:', p2);assert(p2 in [872382,  80,1206])
