beca1aa1dfa47655237b4d24ed2d6ff65d194433
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-12-19 14:23:37 +0100

    sh/both parts - takes forever

--- 09.sh
+++ 09.sh
@@ -25,9 +25,87 @@ makeQ () {
     echo "${res[@]}"
 }
 
+p2() {
+
+    local Q=($(makeQ "$1"))
+    echo "B/init ${Q[*]}" >&2 # init
+
+    local N=${#Q[@]}
+    echo "len/ $N" >&2
+
+    local SPACE=()
+    declare -A IDS
+    local highest=-1
+
+    local i=0
+    while :; do
+        if [[ ${Q[i]} == "." ]]; then
+            local count=0
+            local spaceleft=$i
+            while (( i < N )) && [[ ${Q[i]} == "." ]]; do
+                (( count++ ))
+                (( i++ ))
+            done
+            if (( count != 0 )); then
+                SPACE+=("$spaceleft,$count")
+            fi
+            if (( i >= N )); then
+                break
+            fi
+        else
+            local count=0
+            local spaceleft=$i
+            local curr=${Q[i]}
+            while (( i < N )) && [[ ${Q[i]} == $curr ]]; do
+                (( count++ ))
+                (( i++ ))
+            done
+            if (( count != 0 )); then
+                IDS[$curr]="$spaceleft,$count"
+            fi
+            highest=$curr  # Track the highest value
+            if (( i >= N )); then
+                break
+            fi
+        fi
+    done
+
+    # moving tiles
+    while (( highest > -1 )); do
+        IFS=',' read -r IDleft IDsize <<< "${IDS[$highest]}"
+        for i in "${!SPACE[@]}"; do
+            IFS=',' read -r spaceleft spacesize <<< "${SPACE[$i]}"
+            if (( spaceleft > IDleft )); then
+                break
+            fi
+
+            if (( IDsize == spacesize )); then
+                IDS[$highest]="$spaceleft,$IDsize"
+                SPACE[$i]="$(( spaceleft + IDsize )),0"
+                break
+            fi
+            if (( IDsize < spacesize )); then
+                IDS[$highest]="$spaceleft,$IDsize"
+                SPACE[$i]="$(( spaceleft + IDsize )),$((spacesize - IDsize))"
+                break
+            fi
+        done
+        (( highest-- ))
+    done
+
+    local res=0
+    for key in "${!IDS[@]}"; do
+        IFS=',' read -r L N <<< "${IDS[$key]}"
+        for (( i = L; i < L + N; i++ )); do
+            (( res += i * key ))
+        done
+    done
+    echo "$res"
+}
+
 p1() {
 
-    Q=($(makeQ "$1"))
+    local Q=($(makeQ "$1"))
     echo "A/init ${Q[*]}" >&2 # init
 
     i=0
@@ -50,6 +128,11 @@ p1() {
 }
 
 read -r line
-#res=$(makeQ "$line")
-res=$(p1 "$line")
-echo "part 1:" $res
+
+res1=$(p1 "$line")
+echo "part 1:" $res1
+
+res2=$(p2 "$line");echo
+
+echo "part 1:" $res1 "shouldbe/ 1928 - 6258319840548"
+echo "part 2:" $res2 "shouldbe/ 2858 - 6286182965311"
