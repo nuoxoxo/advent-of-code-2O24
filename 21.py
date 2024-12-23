e047e7d262f508d3231771c6b4848e53b23d4dd2
nuo <49533950+nuoxoxo@users.noreply.github.com>
2024-12-23 01:17:05 +0100

    21/both

--- 21.py
+++ 21.py
@@ -41,7 +41,7 @@ def nextdir(r,c,btn):
 at the A button in the bottom right corner""" # 3,2
 """directional keypad, its robot arm is pointed 
 at the A button in the upper right corner""" # 0,2
-res = 0
+res1 = 0
 import collections
 for l in lines:
     def go(line):
@@ -72,6 +72,80 @@ for l in lines:
                         if nextbtn:
                             updated += nextbtn
                 Q.append( (updated,cost + 1,(rr1,cc1),(rr2,cc2),(rr3,cc3)) )
-    res += go(l) * int(l[:-1])
-print('res/', res)
+    res1 += go(l) * int(l[:-1])
+#print('part 1:', res)
 
+npos = {}
+for r in range(4):
+    for c in range(3):
+        if NUMS[r][c]: npos[NUMS[r][c]] = (r,c)
+
+dpos = {}
+for r in range(2):
+    for c in range(3):
+        if DIRS[r][c]: dpos[DIRS[r][c]] = (r,c)
+
+def paths(S,E,where):
+    def recursebackwards(s,e,where):
+        #print(sr,sc,where)
+        if s == e:
+            return ['A']
+        if where == 'NUM' and s not in npos.values() \
+        or where == 'DIR' and s not in dpos.values():
+            return []
+        res = []
+        sr,sc = s
+        er,ec = e
+        if sr > er:
+            news = (sr - 1, sc)
+            P = recursebackwards(news,e,where)
+            res += [p + '^' for p in P]
+        if sr < er:
+            news = (sr + 1, sc)
+            P = recursebackwards(news,e,where)
+            res += [p + 'v' for p in P]
+        if sc > ec:
+            news = (sr, sc - 1)
+            P = recursebackwards(news,e,where)
+            res += [p + '<' for p in P]
+        if sc < ec:
+            news = (sr, sc + 1)
+            P = recursebackwards(news,e,where)
+            res += [p + '>' for p in P]
+        return res
+    res = recursebackwards(S,E,where)
+    return [_[::-1] for _ in res]
+
+cache = {}
+def p2(skey,ekey,level):
+    if level == 26: return 1
+    if (skey,ekey,level) in cache: return cache[(skey,ekey,level)]
+    res = 10**20
+    if level != 0:
+        spos,epos,where = dpos[skey],dpos[ekey],'DIR'
+    else:
+        spos,epos,where = npos[skey],npos[ekey],'NUM'
+    P = paths(spos,epos,where)
+    for p in P:
+        size = 0
+        start = 'A'
+        for end in p:
+            size += p2(start,end,level + 1)
+            start = end
+        res = min(res,size)
+    cache[(skey,ekey,level)] = res
+    return res
+
+res2 = 0
+for l in lines:
+    size = 0
+    start = 'A'
+    for end in l:
+        size += p2(start, end, 0)
+        start = end
+    res2 += int(l[:3]) * size
+
+print('part 1:', res1)
+assert res1 in[126384,215374]
+print('part 2:', res2)
+assert res2 in[260586897262600]
